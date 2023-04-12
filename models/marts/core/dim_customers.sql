99445f72d8139ea941fad527049996d9a47d6ed2
Fred Yeadon <fwy@qad.com>
2023-04-12 01:35:06 +0000

    add models and sources

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,44 +1,44 @@
 with customers as (
-
     select * from {{ ref('stg_customers') }}
-
 ),
 
 orders as (
-
     select * from {{ ref('stg_orders') }}
+),
 
+payments as (
+    select * from {{ ref('stg_payments') }}
 ),
 
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as order_payments
 
     from orders
+    left join payments using (order_id)
 
     group by 1
-
 ),
 
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(sum(order_payments), 0) as lifetime_value
 
     from customers
-
     left join customer_orders using (customer_id)
 
+    group by 1, 2, 3, 4, 5, 6
+    order by 1
 )
 
 select * from final
